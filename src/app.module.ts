73b7690a3fc3a11cb77f292f5be2ff028092d6aa
SagillaireTW <juansantis@twentic.com>
2026-01-29 02:38:26 +0100

    changes: app.module ssl change

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,18 +18,21 @@ import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
     TypeOrmModule.forRoot({
       ssl: false,
+      extra: {
+        ssl:  false
+      },
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,      
+      password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
     }),
 
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname,'..','public'), 
+      rootPath: join(__dirname, '..', 'public'),
     }),
 
     ProductsModule,
@@ -46,4 +49,4 @@ import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
   ],
 })
-export class AppModule {}
+export class AppModule { }
